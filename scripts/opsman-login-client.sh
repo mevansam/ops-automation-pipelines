cdb81ff778bb278e4e7d0814b373e369edb5567c
Mevan Samaratunga <mevansam@gmail.com>
2017-06-01 07:18:06 +0400

    added tracing

--- scripts/opsman-login-client.sh
+++ scripts/opsman-login-client.sh
@@ -4,4 +4,7 @@ source ~/scripts/iaas-func.sh
 source ~/scripts/opsman-func.sh
 iaas::initialize
 
+[[ -n "$TRACE" ]] && set -x
+set -e
+
 opsman::login_client $OPSMAN_HOST $PCFOPS_CLIENT $PCFOPS_SECRET $OPSMAN_PASSPHRASE
